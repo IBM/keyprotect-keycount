05d54f4827860f6452f46746f620ad19555ac4bd
Kirill-Stakhovsky <kirill.stakhovsky@ibm.com>
2022-06-28 19:52:42 -0500

    revised readme, bugfixes for script

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ The purpose of this repository is to provide customers a way to count the amount
 
 This repository contains binaries for calculating key count to determine the cost of their instance(s).
 
+Instructions for counting key versions in your ibm cloud account (Unix env)
+1. Set in your environment your IBMCLOUD_API_KEY for the account you wish to count key versions in
+2. Run keycount.sh
+    * The program will iterate through all your instances, counting the keyversions for each instance. At the end a total count for all key versions in all regions, in all instances for all keys in the account will be displayed.
+
 * [README.md](README.md)
 * [MAINTAINERS.md](MAINTAINERS.md)
 * [/src](src)
@@ -17,4 +22,7 @@ This repository contains binaries for calculating key count to determine the cos
 ## Notes
 
 Key Versions: The total amount of key material generated for all keys in an instances. 5 Keys created, 3 keys rotated -> Total amount of keys remains 5, total amount of key versions = 8 (5 created + 3 rotated)
-Add notes for how to use the keycount binaries, requirements (apikey, instance id etc). Will update final version when the script is functional per account or per region, currently only is scoped to counting key versions in one instance
+
+Two files: 
+1. countperinstance.txt - keeps track of key versions per instance, useful for tracking which instances have a large amount of keys
+2. instances.txt - keeps track of instances and their regions, used to call keyversions for each instance
--- src/keycount.sh
+++ src/keycount.sh
@@ -7,19 +7,60 @@ else
     APIKEY="${IBMCLOUD_API_KEY}"
 fi
 
-if [[ -z "${BASE_URL}" ]]; then
-   BASE_URL="https://us-south.kms.cloud.ibm.com"
-else
-   BASE_URL="${BASE_URL}"
-fi
-
 ibmcloud login -apikey $APIKEY --no-region
-ibmcloud config --check-version=false
-ibmcloud resource service-instances --long | grep "GUID:" | awk -F "GUID:" '{print $2}' > instances.txt
+ibmcloud resource service-instances --long | grep "GUID:\|Location" | awk '/GUID:/{ thing = $2; } /Location/ { print thing,":",$NF; }' >  instances.txt
+truncate -s -1 instances.txt
 
 rm countperinstance.txt
 touch countperinstance.txt; chmod +x countperinstance.txt
-while read -r line; do go run keycount.go $BASE_URL $APIKEY $line; done < instances.txt
+while read -r line; do 
+    arrLine=(${line//:/})
+    instance=${arrLine[0]}
+    region=${arrLine[1]}
+
+    size=${#instance} 
+
+    if [[ size -gt 36 ]]; then
+        continue
+    fi
+    echo $instance
+    echo $region
+    
+    case $region in
+        "us-south")
+            BASE_URL="https://us-south.kms.cloud.ibm.com"
+            ;;
+        "us-east")
+            BASE_URL="https://us-east.kms.cloud.ibm.com"
+            ;;
+        "br-sao")
+            BASE_URL="https://br-sao.kms.cloud.ibm.com"
+            ;;
+        "au-syd")
+            BASE_URL="https://au-syd.kms.cloud.ibm.com"
+            ;;
+        "jp-tok")
+            BASE_URL="https://jp-tok.kms.cloud.ibm.com"
+            ;;
+        "ca-tor")
+            BASE_URL="https://ca-tor.kms.cloud.ibm.com"
+            ;;
+        "jp-osa")
+            BASE_URL="https://jp-osa.kms.cloud.ibm.com"
+            ;;
+        "eu-gb")
+            BASE_URL="https://eu-gb.kms.cloud.ibm.com"
+            ;;
+        "eu-de")
+            BASE_URL="https://eu-de.kms.cloud.ibm.com"
+            ;;
+        *)
+            continue
+            ;;
+    esac
+
+    go run keycount.go $BASE_URL $APIKEY ${arrLine[0]};
+done < instances.txt
 
 totalsum=0
 while read -r line; do
